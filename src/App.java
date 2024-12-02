2b12491a8eb61b6f36c8b72e9e7069ae46e3241e
Naufal Andrianto Nugraha <andriantonugrahanaufal@gmail.com>
2024-12-02 17:55:02 +0700

    Tambah tampilan awal bioskop
    Ketika masuk kedalam program, pengguna dapat memilih,
    masuk ke fitur penonton atau pegawai bioskop. Apabila
    pengguna memasukkan opsi yang tidak tersedia, akan
    menampilkan pesan bahwa opsi tersebut tidak tersedia.
    Dan menambahkan class untuk membersihkan terminal.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
+import utils.ScreenCleaning;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var scanner = new Scanner(System.in);
+
+        ScreenCleaning.ClearScreen();
+        System.out.println("== Selamat Datang di Nova Cinema ==");
+
+        var isRunning = true;
+        while (isRunning) {
+            System.out.println("----------------------------------------");
+            System.out.println("Berikut merupakan opsi yang tersedia");
+            System.out.println("(1). Masuk sebagai penonton");
+            System.out.println("(2). Masuk sebagai pegawai");
+            System.out.println("(3). Keluar dari program");
+            System.out.println("----------------------------------------");
+
+            System.out.print("- Input: ");
+            var inputOption = scanner.nextInt();
+
+            switch (inputOption) {
+                case 1:
+                    ScreenCleaning.ClearScreen();
+                    System.out.println("Masuk sebagai penonton");
+                    break;
+                case 2:
+                    ScreenCleaning.ClearScreen();
+                    System.out.println("Masuk sebagai pegawai");
+                    break;
+                case 3:
+                    ScreenCleaning.ClearScreen();
+                    System.out.println("-----------------------------");
+                    System.out.println("| Telah keluar dari program |");
+                    System.out.println("-----------------------------");
+                    isRunning = false;
+                    break;
+                default:
+                    ScreenCleaning.ClearScreen();
+                    System.out.println("-------------------------------------");
+                    System.out.println("| Opsi tidak valid                  |");
+                    System.out.println("| Mohon masukkan opsi yang tersedia |");
+                    System.out.println("-------------------------------------");
+                    continue;
+            }
+        }
+        scanner.close();
     }
 }
---
+++ src/utils/ScreenCleaning.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class ScreenCleaning {
+    public static void ClearScreen() {
+        System.out.print("\033\143");
+    }
+}
