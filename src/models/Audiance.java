1651936a3a4c4bb5fc4ee15e04a99b8de59373bc
Naufal Andrianto Nugraha <andriantonugrahanaufal@gmail.com>
2024-12-03 10:08:33 +0700

    Tambah fitur beli tiket
    Pengguna dapat membeli tiket, dengan memasukkan input
    berupa nama, umur, dan gender.

---
+++ src/models/Audiance.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Audiance extends Person {
+    private boolean haveTicket = false;
+
+    public Audiance(
+        String name,
+        int age,
+        String gender,
+        boolean haveTicket
+    ) {
+        super(name, age, gender);
+        this.haveTicket = haveTicket;
+    }
+
+    @Override
+    public void showInformation() {
+        System.out.println("--------------------------------");
+        System.out.println("Name  : " + this.name);
+        System.out.println("Age   : " + this.age);
+        System.out.println("Gender: " + this.gender);
+        System.out.println("Ticket: " + this.haveTicket);
+        System.out.println("--------------------------------");
+    }
+
+    public void setHaveTicket(boolean haveTicket) {
+        this.haveTicket = haveTicket;
+    }
+
+    public boolean getHaveTicket() {
+        return this.haveTicket;
+    }
+}
---
+++ src/models/Person.java
@@ -0,0 +1,45 @@
+package models;
+
+public class Person {
+    protected String name;
+    protected int age;
+    protected String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void showInformation() {
+        System.out.println("--------------------------------");
+        System.out.println("Name  : " + this.name);
+        System.out.println("Age   : " + this.age);
+        System.out.println("Gender: " + this.gender);
+        System.out.println("--------------------------------");
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setGender(String newGender) {
+        this.gender = newGender;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+}
